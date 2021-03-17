92127e4f9172abd5d5e05d5188920d9b21e62267
KovacsMarton <43884219+KovacsMarton@users.noreply.github.com>
2021-03-17 12:00:28 +0100

    Home changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Ad } from '../models/ad';
+import { Property } from '../models/property';
 
 
 @Component({
@@ -7,8 +10,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  private example_property: Property = {property_id: 1,ad:null,roomNumber: 5,type:"jo",state:"omladozik a haz",size:1234 }
 
-  constructor() { }
+  private example_ad: Ad = { ad_id: 1, property: this.example_property, picture: "https://www.propertysupport.hu/wp-content/uploads/2020/11/property-support-ingatlan-tanacsadas-1.jpg", price: "1500000 Ft", location: "Budapest ...", details: null };
+
+public ads: Ad[] = [this.example_ad]
+
+  constructor(private service: AuthService) { }
 
   ngOnInit(): void {
   }
