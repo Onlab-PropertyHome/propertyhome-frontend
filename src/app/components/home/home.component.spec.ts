3cee619b8f05a3375c12d95e5887af33dc46837f
Adam <61555703+akunsecured@users.noreply.github.com>
2021-05-04 19:07:17 +0200

    Fixing errors

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,14 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { of } from 'rxjs';
+import { AdvertisementService } from 'src/app/services/advertisement.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  const advertisementServiceStub = {
+
+  };
+  const authServiceStub = {
+
+  };
+  const formBuilderStub = {
+    group: () => of()
+  };
+  const modalServiceStub = {
+
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      providers: [
+        { provide: AdvertisementService, useValue: advertisementServiceStub },
+        { provide: AuthService, useValue: authServiceStub },
+        { provide: FormBuilder, useValue: formBuilderStub },
+        { provide: NgbModal, useValue: modalServiceStub }
+      ]
     })
     .compileComponents();
   });
--- src/app/services/advertisement.service.spec.ts
+++ src/app/services/advertisement.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { AdvertisementService } from './advertisement.service';
@@ -6,7 +7,11 @@ describe('AdvertisementService', () => {
   let service: AdvertisementService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ]
+    });
     service = TestBed.inject(AdvertisementService);
   });
 
